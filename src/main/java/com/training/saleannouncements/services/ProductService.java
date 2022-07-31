9c99287134261ef2ce114b23828956def5ea2930
isamo <isamo@softserveinc.com>
2022-07-31 18:39:32 +0300

    Make empty bin files to be not saved

--- src/main/java/com/training/saleannouncements/services/ProductService.java
+++ src/main/java/com/training/saleannouncements/services/ProductService.java
@@ -8,7 +8,6 @@ import com.training.saleannouncements.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -34,7 +33,9 @@ public class ProductService {
         Image image;
         if (!product.getTitle().isBlank() && product.getPrice() != null) {
             for (int i = 0; i < imageFiles.size(); i++) {
-                image = toImageEntity(imageFiles.get(i));
+                if (imageFiles.get(i).getSize() != 0) {
+                    image = toImageEntity(imageFiles.get(i));
+                } else continue;
                 if (i == 0) {
                     image.setPreviewImage(true);
                 }
